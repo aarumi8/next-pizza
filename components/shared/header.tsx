913d2da0fcb5eef93ac6771f165487e8c3ac68b4
Max Rand <dopler168@gmail.com>
2024-09-26 16:22:56 +0300

    add api client and search

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from "./container";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 interface Props {
     className?: string;
@@ -13,13 +15,18 @@ export const Header: React.FC<Props> = ({ className }) => {
     return (
         <header className={cn('border border-b', className)}>
             <Container className="flex items-center justify-between py-8">
-
+                <Link href='/'>
                 <div className="flex items-center gap-4">
                     <Image src="/logo.png" alt="logo" width={35} height={35} />
                     <div>
                         <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
                         <p className="text-sm text-gray-400 leading-3">mmm, so much tasty!</p>
                     </div>
+                </div>
+                </Link>
+
+                <div className="mx-10 flex-1">
+                    <SearchInput />
                 </div>
 
                 <div className="flex items-center gap-3">
