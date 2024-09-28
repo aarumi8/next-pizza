5beaded1877c895a7dc92557ca4fe3fadc1998fa
Max Rand <dopler168@gmail.com>
2024-09-28 16:38:28 +0300

    add product modals

---
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+import { ChooseProductModal } from "@/components/shared/modals/choose-product-modal";
+
+export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+    const product = await prisma.product.findFirst({
+        where: {
+            id: Number(id),
+        },
+        include: {
+            ingredients: true,
+            items: true,
+        },
+    });
+
+    if (!product) {
+        return notFound();
+    }
+
+    return (
+        <ChooseProductModal product={product} />
+    );
+}
